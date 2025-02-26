b5cae6df3ff5fbc00127e7528f225b3a55b6d37b
MrG00gle <mateuszkuzmiankou@gmail.com>
2025-02-26 21:52:29 +0100

    test

--- modules/home-manager/modules/gnome.nix
+++ modules/home-manager/modules/gnome.nix
@@ -144,15 +144,15 @@
             name = "File-Manager";
           };
 
-          "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom1" = {
-            binding = "<Control><Alt>t";
-            command = "/usr/bin/env gnome-console";
-            name = "Terminal";
-          };
+          # "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom1" = {
+          #   binding = "<Control><Alt>t";
+          #   command = "/usr/bin/env gnome-console";
+          #   name = "Terminal";
+          # };
 
           "org/gnome/settings-daemon/plugins/media-keys".custom-keybindings = [
             "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/"
-            "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom1/"
+            # "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom1/"
           ];
 
       };
