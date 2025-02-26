45ac1868a477fd1e1dbb377aabc3895c941629c0
MrG00gle <mateuszkuzmiankou@gmail.com>
2025-02-26 13:09:45 +0100

    dconf path fix

--- modules/gnome.nix
+++ modules/gnome.nix
@@ -60,7 +60,7 @@
 
         # Extensions
         "/org/gnome/shell/extensions/dash-to-dock" = {
-          application-counter-overrides-notifications=true;
+          # application-counter-overrides-notifications=true;
           background-color = "rgb(36,36,36)";
           background-opacity = gvariant.mkDouble 0.80000000000000004;
           click-action = "minimize";
