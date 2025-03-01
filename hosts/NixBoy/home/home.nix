ee2621a471971d53e53bff8ffe147f72e47fd449
MrG00gle <mateuszkuzmiankou@gmail.com>
2025-03-01 11:47:12 +0100

    testting config 1

--- hosts/NixBoy/home/home.nix
+++ hosts/NixBoy/home/home.nix
@@ -1,19 +1,26 @@
 { config, pkgs, ... }:
-let
-  home-manager = builtins.fetchTarball "https://github.com/nix-community/home-manager/archive/release-24.11.tar.gz";
-in
+# let
+#   home-manager = builtins.fetchTarball "https://github.com/nix-community/home-manager/archive/release-24.11.tar.gz";
+# in
 {
   imports = [
     (import "${home-manager}/nixos")
     ./home-packages.nix
     ./apps/git.nix
-    ./apps/gnome.nix
+    ./apps/wm/gnome.nix
     ./apps/shell.nix
     ./apps/steam.nix
   ];
-  home-manager.users.mrgoogle = {
-    /* The home.stateVersion option does not have a default and must be set */
-    home.stateVersion = "24.11";
-    programs.home-manager.enable = true;
-  };
+
+  home.username = "mrgoogle";
+  home.homeDirectory = "/home/mrgoogle";
+  home.stateVersion = "24.11";
+  programs.home-manager.enable = true;
+
+
+  # home-manager.users.mrgoogle = {
+  #   /* The home.stateVersion option does not have a default and must be set */
+  #   home.stateVersion = "24.11";
+  #   programs.home-manager.enable = true;
+  # };
 }
