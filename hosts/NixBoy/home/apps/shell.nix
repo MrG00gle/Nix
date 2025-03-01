de01c396763037516c5dbbe3b5a8ef2402cc77ae
MrG00gle <mateuszkuzmiankou@gmail.com>
2025-03-01 19:02:01 +0100

    syntax fix: shell.nix 1

--- hosts/NixBoy/home/apps/shell.nix
+++ hosts/NixBoy/home/apps/shell.nix
@@ -1,29 +1,29 @@
 { config, ... }: {
 
-  programs.zsh = {
-    enable = true;
-    enableCompletion = true;
-    # autosuggestions.enable = true;
-    syntaxHighlighting.enable = true;
-    # histSize = 10000;
+  # programs.zsh = {
+  #   enable = true;
+  #   enableCompletion = true;
+  #   # autosuggestions.enable = true;
+  #   syntaxHighlighting.enable = true;
+  #   # histSize = 10000;
 
-    shellAliases = {
-      ll = "ls -l";
-      cl = "clear";
-      update = "sudo nixos-rebuild switch |& nom";
-      upgrade = "sudo nixos-rebuild switch --upgrade |& nom";
-      test = "sudo nixos-rebuild test |& nom";
-      test-upgrade = "sudo nixos-rebuild test --upgrade |& nom";
-      test-trace = "sudo nixos-rebuild test --show-trace";
-      pingtest = "ping 8.8.8.8";
-    };
+  #   shellAliases = {
+  #     ll = "ls -l";
+  #     cl = "clear";
+  #     update = "sudo nixos-rebuild switch |& nom";
+  #     upgrade = "sudo nixos-rebuild switch --upgrade |& nom";
+  #     test = "sudo nixos-rebuild test |& nom";
+  #     test-upgrade = "sudo nixos-rebuild test --upgrade |& nom";
+  #     test-trace = "sudo nixos-rebuild test --show-trace";
+  #     pingtest = "ping 8.8.8.8";
+  #   };
 
-    ohMyZsh = {
-      enable = true;
-      plugins = [ "git" "history" "zsh-autosuggestions" "zsh-syntax-highlighting" "docker" "sudo"];
-      theme = "powerlevel10k/powerlevel10k";
-    };
-};
+  #   ohMyZsh = {
+  #     enable = true;
+  #     plugins = [ "git" "history" "zsh-autosuggestions" "zsh-syntax-highlighting" "docker" "sudo"];
+  #     theme = "powerlevel10k/powerlevel10k";
+  #   };
+  # };
 
   programs.fish = {
     enable = true;
