ac1e558a42b36853f6182973358c77f45bbd6cf3
MrG00gle <mateuszkuzmiankou@gmail.com>
2025-08-08 21:03:24 +0200

    services.nix fixing

--- hosts/nixos/system/services.nix
+++ hosts/nixos/system/services.nix
@@ -9,13 +9,6 @@
   };
 
   services.flatpak.enable = true;
-
-    # Add a new remote. Keep the default one (flathub)
-  services.flatpak.remotes = lib.mkOptionDefault [{
-    name = "flathub-beta";
-    location = "https://flathub.org/beta-repo/flathub-beta.flatpakrepo";
-  }];
-
   services.flatpak.update.auto.enable = false;
   services.flatpak.uninstallUnmanaged = false;
 
