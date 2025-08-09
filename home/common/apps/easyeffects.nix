2a50f5df102a178b7f76aa7fd6259186325cc624
MrG00gle <mateuszkuzmiankou@gmail.com>
2025-08-09 18:10:03 +0200

    Quick fix for easyeffects.nix

--- home/common/apps/easyeffects.nix
+++ home/common/apps/easyeffects.nix
@@ -1,7 +1,7 @@
 {lib, ...}: {
 
 #  programs.dconf.enable = true;
-  home.options.services.easyeffects.extraPresets = {
+  options.services.easyeffects.extraPresets = {
     tonor-tc20 = {
       input = {
         blocklist = [];
