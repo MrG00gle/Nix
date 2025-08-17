fab5c2cc394b77700ea7d5853749376c2f3b0810
Matsvei Kuzmiankou <105366428+MrG00gle@users.noreply.github.com>
2025-08-17 20:21:24 +0200

    sound fix

--- hosts/NixBoy/system/sound.nix
+++ hosts/NixBoy/system/sound.nix
@@ -9,42 +9,42 @@
   };
 
   # Enbling codecs for wireless headphones
-  services.pipewire.wireplumber.extraConfig.bluetoothEnhancements = {
-    "monitor.bluez.properties" = {
-        "bluez5.enable-sbc-xq" = true;
-        "bluez5.enable-msbc" = true;
-        "bluez5.enable-hw-volume" = true;
-        "bluez5.roles" = [ "hsp_hs" "hsp_ag" "hfp_hf" "hfp_ag" ];
-    };
-  };
+  # services.pipewire.wireplumber.extraConfig.bluetoothEnhancements = {
+  #   "monitor.bluez.properties" = {
+  #       "bluez5.enable-sbc-xq" = true;
+  #       "bluez5.enable-msbc" = true;
+  #       "bluez5.enable-hw-volume" = true;
+  #       "bluez5.roles" = [ "hsp_hs" "hsp_ag" "hfp_hf" "hfp_ag" ];
+  #   };
+  # };
 
-  services.pipewire.extraConfig.pipewire."92-low-latency" = {
-    "context.properties" = {
-      "default.clock.rate" = 48000;
-      "default.clock.quantum" = 32;
-      "default.clock.min-quantum" = 32;
-      "default.clock.max-quantum" = 32;
-    };
-  };
+  # services.pipewire.extraConfig.pipewire."92-low-latency" = {
+  #   "context.properties" = {
+  #     "default.clock.rate" = 48000;
+  #     "default.clock.quantum" = 32;
+  #     "default.clock.min-quantum" = 32;
+  #     "default.clock.max-quantum" = 32;
+  #   };
+  # };
 
 
-  services.pipewire.extraConfig.pipewire-pulse."92-low-latency" = {
-    "context.properties" = [
-      {
-        name = "libpipewire-module-protocol-pulse";
-        args = { };
-      }
-    ];
-    "pulse.properties" = {
-      "pulse.min.req" = "32/48000";
-      "pulse.default.req" = "32/48000";
-      "pulse.max.req" = "32/48000";
-      "pulse.min.quantum" = "32/48000";
-      "pulse.max.quantum" = "32/48000";
-    };
-    "stream.properties" = {
-      "node.latency" = "32/48000";
-      "resample.quality" = 1;
-    };
-  };
+  # services.pipewire.extraConfig.pipewire-pulse."92-low-latency" = {
+  #   "context.properties" = [
+  #     {
+  #       name = "libpipewire-module-protocol-pulse";
+  #       args = { };
+  #     }
+  #   ];
+  #   "pulse.properties" = {
+  #     "pulse.min.req" = "32/48000";
+  #     "pulse.default.req" = "32/48000";
+  #     "pulse.max.req" = "32/48000";
+  #     "pulse.min.quantum" = "32/48000";
+  #     "pulse.max.quantum" = "32/48000";
+  #   };
+  #   "stream.properties" = {
+  #     "node.latency" = "32/48000";
+  #     "resample.quality" = 1;
+  #   };
+  # };
 }
