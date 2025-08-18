8bfc3f003513da8065cf50e7a80758e8e868e042
MrG00gle <mateuszkuzmiankou@gmail.com>
2025-08-18 15:51:30 +0200

    test

--- hosts/NixBoy/system/bluetooth.nix
+++ hosts/NixBoy/system/bluetooth.nix
@@ -1,22 +1,22 @@
 {
   # Enable Bluetooth
-  # hardware.bluetooth.enable = true;         # enables support for Bluetooth
+  hardware.bluetooth.enable = true;         # enables support for Bluetooth
   # hardware.bluetooth.settings.General.Experimental = true;    # Showing battery charge of bluetooth devices 
   # hardware.bluetooth.powerOnBoot = true;    # powers up the default Bluetooth controller on boot
   # services.blueman.enable = true;           # Optional: Blueman GUI for managing Bluetooth
-    hardware.bluetooth = {
-    enable = true;
-    settings = {
-      General = {
-        Name = "Hello";
-        ControllerMode = "dual";
-        FastConnectable = "true";
-        Experimental = "true";
-      };
-      Policy = {
-        AutoEnable = "true";
-      };
-    };
-  };
+  #   hardware.bluetooth = {
+  #   enable = true;
+  #   settings = {
+  #     General = {
+  #       Name = "Hello";
+  #       ControllerMode = "dual";
+  #       FastConnectable = "true";
+  #       Experimental = "true";
+  #     };
+  #     Policy = {
+  #       AutoEnable = "true";
+  #     };
+  #   };
+  # };
 
 }
