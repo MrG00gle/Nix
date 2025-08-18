aeb153ec3f391fb08254b579135ff8f335534a36
MrG00gle <mateuszkuzmiankou@gmail.com>
2025-08-18 15:16:11 +0200

    trying to fix bluetooth

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
 
 # Declaring package sources
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";#"github:nixos/nixpkgs/nixos-25.05";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
     # nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
     nix-flatpak.url = "github:gmodena/nix-flatpak";
     disko = {
