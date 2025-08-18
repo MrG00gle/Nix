1e64b6b15bbe4ad7ed0972a191d49c096151d9f9
MrG00gle <mateuszkuzmiankou@gmail.com>
2025-08-18 15:48:54 +0200

    refactor

--- hosts/NixBoy/hardware-configuration.nix
+++ hosts/NixBoy/hardware-configuration.nix
@@ -10,7 +10,8 @@
 
   boot.initrd.availableKernelModules = [ "xhci_pci" "nvme" "ahci" "usb_storage" "usbhid" "btusb" "sd_mod" ];
   boot.initrd.kernelModules = [ ];
-  boot.kernelModules = [ "kvm-amd" "btusb" "rtw89" "brcmfmac" "brcmutil" "iwlmvm" "iwlwifi" "mmc_core" "mt76_usb" "mt76" "mt76x0_common" "mt76x02_lib" "mt76x02_usb" "mt76x0u" "r8188eu" "rtl_usb" "rtl8192c_common" "rtl8192cu" "rtlwifi" ];  boot.extraModulePackages = [ ];
+  boot.kernelModules = [ "kvm-amd" "btusb" "rtw89" "brcmfmac" "brcmutil" "iwlmvm" "iwlwifi" "mmc_core" "mt76_usb" "mt76" "mt76x0_common" "mt76x02_lib" "mt76x02_usb" "mt76x0u" "r8188eu" "rtl_usb" "rtl8192c_common" "rtl8192cu" "rtlwifi" ];  
+  boot.extraModulePackages = [ ];
   boot.kernelPackages = pkgs.linuxPackages_latest;
 
 
