d0e5e4c555518ed27f31dd5f59283c64d309632e
MrG00gle <mateuszkuzmiankou@gmail.com>
2025-08-19 17:35:58 +0200

    Adding aliase.

--- home/common/apps/zsh.nix
+++ home/common/apps/zsh.nix
@@ -21,7 +21,7 @@ programs.zsh = {
       lg = "lazygit";
       pingtest = "ping 8.8.8.8";
       rebuild = "sudo nixos-rebuild switch --flake ~/Nix#NixBoy --no-write-lock-file --log-format internal-json -v |& nom --json";
-      
+      test = "sudo nixos-rebuild test --flake ~/Nix#NixBoy --no-write-lock-file --log-format internal-json -v |& nom --json";
     };
     history = {
       expireDuplicatesFirst = true;
