6bf3a541a1c469597dc723215a723c9fa727bfdd
MrG00gle <mateuszkuzmiankou@gmail.com>
2025-09-09 18:59:48 +0200

    Adding kernel modules modules

--- hosts/NixBoy/hardware-configuration.nix
+++ hosts/NixBoy/hardware-configuration.nix
@@ -8,8 +8,8 @@
     ];
 
   boot.initrd.availableKernelModules = [ "xhci_pci" "nvme" "ahci" "usb_storage" "usbhid" "sd_mod" ];
-  boot.initrd.kernelModules = [ ];
-  boot.kernelModules = [ "kvm-amd" ];
+  boot.initrd.kernelModules = [];
+  boot.kernelModules = [ "kvm-amd" "vboxdrv" "vboxnetadp" "vboxnetflt" ];
   boot.extraModulePackages = [ ];
   # boot.kernelPackages = pkgs.linuxPackages_latest;
 
